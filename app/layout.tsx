0910a3be12af009b12802cf1cdec927b5b0906e7
hadiikaddour <hadyqdwr77@gmail.com>
2026-03-26 00:04:22 +0300

    some touches

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'TikTok Clone',
-  description: 'تطبيق TikTok Clone مبني باستخدام Next.js و TailwindCSS',
+  title: 'PrimeCash',
+  description: 'PrimeCash',
 };
 
 export default function RootLayout({
@@ -15,6 +16,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  
+
   return (
     <html lang="ar" dir="rtl">
       <body className={`${inter.className} bg-black text-white`}>
