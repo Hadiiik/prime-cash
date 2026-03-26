c339b4f0c1c5e2320cb3e2279ea71431fe694166
hadiikaddour <hadyqdwr77@gmail.com>
2026-03-26 17:53:01 +0300

    reg sw.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { useEffect } from 'react';
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,12 +17,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
+  useEffect(() => {
+          if (typeof navigator.serviceWorker !== 'undefined') {
+            navigator.serviceWorker.register('sw.js')
+          }
+  }, [])
 
   return (
     <html lang="ar" dir="rtl">
       <body className={`${inter.className} bg-black text-white`}>
         {children}
+        
       </body>
     </html>
   );
